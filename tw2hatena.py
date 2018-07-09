b9772fd8ab9b1b8065859601a234480be3c8db98
Ubuntu <katsumi@katsumi.wpac4x1hui3uthvkjjsep3ph0h.bx.internal.cloudapp.net>
2018-07-09 13:53:14 +0000

    URLの展開

--- tw2hatena.py
+++ tw2hatena.py
@@ -1,4 +1,4 @@
-import json, sys, config
+import json, sys, config, re
 from requests_oauthlib import OAuth1Session
 
 CK = config.CONSUMER_KEY
@@ -9,14 +9,52 @@ tw = OAuth1Session(CK, CS, AT, ATS)
 
 baseurl = "https://api.twitter.com/1.1"
 favorites_url = baseurl + "/favorites/list.json"
+tweet_url = baseurl + "/search/tweets.json"
 
 def getFavorites(name):
 	res = tw.get(favorites_url, params = { 'screen_name': name })
 	tweets = json.loads(res.text)
 	return tweets 
 
-tw = getFavorites(sys.argv[1])
+def getTweets(name):
+	res = tw.get(tweet_url, params = { 'q': "from:" + name })
+	tweets = json.loads(res.text)
+	return tweets 
 
-for item in tw:
-    print(item['text'])
+def expandURL(url):
+	conn = http.client.HTTPSConnection("t.co")
+	conn.rquest("GET", url)
+	res = conn.getresponse()
+	if res.status == 301:
+		hdr = res.getheader("location")
+		return hdr
+	else:
+		return None
 
+def getURL(tw):
+	if tw in "https://t.co/":
+		url = re.findall("https://t.co/[A-Za-z0-9]", tw)
+		return expandURL(url)
+	else:
+		return None
+	
+
+# ふぁぼったURLをブクマ
+entries = getFavorites(sys.argv[1])
+
+for item in entries:
+    tw = item['text']
+    url = getUrl(tw)
+    if url != None:
+        bookmark(url, "")
+
+
+# RT/tweetしたURLをブクマ
+entries = getTweets(sys.argv[1])
+
+results = entries['statuses']
+for item in results:
+    tw = item['text']
+    url = getUrl(tw)
+    if url != None:
+        bookmark(url, tw)
